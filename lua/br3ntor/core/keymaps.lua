087bf37610fa8e9603548cd4c68e92e875ef5839
brent <br3ntor@gmail.com>
2024-02-29 03:21:18 -0800

    tweakin stuff

--- lua/br3ntor/core/keymaps.lua
+++ lua/br3ntor/core/keymaps.lua
@@ -6,16 +6,16 @@ local keymap = vim.keymap -- for conciseness
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
--- Open netrw, the vim file explorer 
+-- Open netrw, the vim file explorer
 -- Hmmm I don't know which way I prefer
 -- keymap.set("n", "<leader>e", vim.cmd.Explore)
 keymap.set("n", "<leader>e", ":Explore<CR>")
 
 -- Buffer controls
-keymap.set("n", "<leader>b", "<cmd>buffers<CR>:buffer<Space>", {desc="Open buffers and prompt choice"})
-keymap.set('n', '<leader>p', ':bprevious<CR>', {})
-keymap.set('n', '<leader>n', ':bnext<CR>', {})
-keymap.set('n', '<leader>q', ':bdelete<CR>', {})
+keymap.set("n", "<leader>b", "<cmd>buffers<CR>:buffer<Space>", { desc = "Open buffers and prompt choice" })
+keymap.set("n", "<leader>p", ":bprevious<CR>", {})
+keymap.set("n", "<leader>n", ":bnext<CR>", {})
+keymap.set("n", "<leader>q", ":bdelete<CR>", {})
 
 -- These are for bufferline plugin, trying all natural atm
 --vim.keymap.set('n', '<leader>p', ':BufferLineCyclePrev<CR>', {})
--- lua/br3ntor/plugins/lsp/lspconfig.lua
+++ lua/br3ntor/plugins/lsp/lspconfig.lua
@@ -165,5 +165,11 @@ return {
 			capabilities = capabilities,
 			on_attach = on_attach,
 		})
+
+		-- configure c
+		lspconfig["clangd"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 	end,
 }
