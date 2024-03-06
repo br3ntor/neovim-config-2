bdf77ff87ede933edbfc7dacef8864fb0c09cf6d
brent <br3ntor@gmail.com>
2024-03-05 23:39:15 -0800

    Added treesitter-context and removed some {}

--- lua/br3ntor/plugins/treesitter.lua
+++ lua/br3ntor/plugins/treesitter.lua
@@ -1,39 +1,37 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-				ensure_installed = {
-					"c",
-					"json",
-					"javascript",
-					"typescript",
-					"tsx",
-					"yaml",
-					"html",
-					"css",
-					"prisma",
-					"markdown",
-					"markdown_inline",
-					"svelte",
-					"graphql",
-					"bash",
-					"lua",
-					"vim",
-					"vimdoc",
-					"dockerfile",
-					"gitignore",
-					"query",
-					"python",
-				},
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"vimdoc",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"python",
+			},
 
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
