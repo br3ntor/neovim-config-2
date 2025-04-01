d2d4fca58d0d58dacfb4f52c894f2114efc347f9
brent <br3ntor@gmail.com>
2025-03-31 17:38:40 -0700

    I did stuff

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
-require("br3ntor.core")
-require("br3ntor.lazy")
-print("Welcome to br3ntor's neovim setup!")
-print("Settings and Plugins loaded!")
+if vim.g.vscode then
+	-- VSCode extension
+	print("We in VSCode wow!")
+	-- vim.keymap.set("v", "<C-c>", '"+y', { noremap = true, silent = true })
+else
+	require("br3ntor.core")
+	require("br3ntor.lazy")
+	print("Welcome to br3ntor's neovim setup!")
+	print("Settings and Plugins loaded!")
+	-- ordinary Neovim
+end
